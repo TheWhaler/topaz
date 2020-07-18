8e86bb77eb44f89f093b85a0bc5eb70db48d902b
TheWhaler <jmwv18@gmail.com>
2020-07-18 14:54:06 -0400

    fix for embrace not working on mob

--- scripts/globals/mobskills/plenilune_embrace.lua
+++ scripts/globals/mobskills/plenilune_embrace.lua
@@ -1,21 +1,20 @@
 ---------------------------------------------
---  Plenilune Embrace
---
---  Description: Restores HP for the target party member. & adds att + def buff 
---  
---
+-- Plenilune_Embrace
 --
 ---------------------------------------------
-require("/scripts/globals/settings");
-require("/scripts/globals/status");
-require("/scripts/globals/monstertpmoves");
+require("scripts/globals/monstertpmoves")
+require("scripts/globals/status")
+require("scripts/globals/msg")
 ---------------------------------------------
-function OnMobSkillCheck(target,mob,skill)
-	return 0;
-end;
 
-function OnMobWeaponSkill(target, mob, skill)
+function onMobSkillCheck(target,mob,skill)
+    return 0
+end
 
+function onMobWeaponSkill(target, mob, skill)
+    -- Saw no math at any online source describing mob version, only player version..
+    -- Someone needs to go figure out what the retail math is.
+	
 	local potency = skill:getParam();
 
     if(potency == 0) then
@@ -26,8 +25,8 @@ function OnMobWeaponSkill(target, mob, skill)
 
 	skill:setMsg(MSG_SELF_HEAL);
 	
-	MobPhysicalStatusEffectMove(mob, target, skill, tpz.effect.ATTACK_BOOST, 25, 0, 300);
-	MobPhysicalStatusEffectMove(mob, target, skill, tpz.effect.MAGIC_ATK_BOOST, 25, 0, 300);
+	MobPhysicalStatusEffectMove(mob, target, skill, EFFECT_ATTACK_BOOST, 25, 0, 300);
+	MobPhysicalStatusEffectMove(mob, target, skill, EFFECT_MAGIC_ATK_BOOST, 25, 0, 300);
 	
 	return MobHealMove(mob, mob:getMaxHP() * potency / 100);
-end;
+end
