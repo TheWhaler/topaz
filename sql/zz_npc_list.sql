84dd299d1177a431772e67f2d00eeb288b4ada37
Ian <ian.evans1188@gmail.com>
2020-07-19 09:00:12 -0700

    Removed --TRUST NPC. Was receiving errors on HeidiSQL when trying to run.

--- sql/zz_npc_list.sql
+++ sql/zz_npc_list.sql
@@ -1,4 +1,4 @@
 DELETE FROM npc_list where name = 'Survival_Guide';
-UPDATE npc_list SET content_tag = NULL WHERE (`npcid` = '17764797'); --TRUST STARTER
-UPDATE npc_list SET content_tag = NULL WHERE (`npcid` = '17719943'); --TRUST STARTER
-UPDATE npc_list SET content_tag = NULL WHERE (`npcid` = '17744187'); --TRUST STARTER
+UPDATE npc_list SET content_tag = NULL WHERE (`npcid` = '17764797');
+UPDATE npc_list SET content_tag = NULL WHERE (`npcid` = '17719943');
+UPDATE npc_list SET content_tag = NULL WHERE (`npcid` = '17744187');
