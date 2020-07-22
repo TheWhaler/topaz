799c7085d29f10c3d8f82e0e774fd13a8dfa3a07
TheWhaler <jmwv18@gmail.com>
2020-07-22 18:46:45 -0400

    mapitoto

--- sql/zz_npc_list.sql
+++ sql/zz_npc_list.sql
@@ -2,3 +2,4 @@ DELETE FROM npc_list where name = 'Survival_Guide';
 UPDATE npc_list SET content_tag = NULL WHERE (`npcid` = '17764797');
 UPDATE npc_list SET content_tag = NULL WHERE (`npcid` = '17719943');
 UPDATE npc_list SET content_tag = NULL WHERE (`npcid` = '17744187');
+UPDATE npc_list SET content_tag = NULL WHERE (`npcid` = '17776895');
