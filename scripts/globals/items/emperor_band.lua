8a1b1269bd1a5297d868326e1969df2560eb772b
TheWhaler <jmwv18@gmail.com>
2020-07-26 12:30:41 -0400

    Add back exp rings era buffs

--- scripts/globals/items/emperor_band.lua
+++ scripts/globals/items/emperor_band.lua
@@ -12,12 +12,12 @@ require("scripts/globals/status")
 
 function onItemCheck(target)
     local result = 0
-    if (target:hasStatusEffect(tpz.effect.DEDICATION) == true) then
+    if (target:hasStatusEffect(tpz.effect.DEDICATION) == true) or target:getZoneID() == (15 or 45 or 132 or 215 or 216 or 217 or 218 or 253 or 254 or 255) then
         result = 56
     end
     return result
 end
 
 function onItemUse(target)
-    target:addStatusEffect(tpz.effect.DEDICATION,50,0,43200,0,30000)
+    target:addStatusEffect(tpz.effect.DEDICATION,50,0,43200,0,2000)
 end
--- scripts/globals/items/empress_band.lua
+++ scripts/globals/items/empress_band.lua
@@ -12,12 +12,12 @@ require("scripts/globals/status")
 
 function onItemCheck(target)
     local result = 0
-    if (target:hasStatusEffect(tpz.effect.DEDICATION) == true) then
+    if (target:hasStatusEffect(tpz.effect.DEDICATION) or target:getZoneID() == (15 or 45 or 132 or 215 or 216 or 217 or 218 or 253 or 254 or 255) == true) then
         result = 56
     end
     return result
 end
 
 function onItemUse(target)
-    target:addStatusEffect(tpz.effect.DEDICATION,50,0,43200,0,15000)
+    target:addStatusEffect(tpz.effect.DEDICATION,50,0,43200,0,1000)
 end
