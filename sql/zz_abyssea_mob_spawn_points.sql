b8841691f727fe2aff43efebac4d3c077ebf6724
Ian <ian.evans1188@gmail.com>
2020-08-21 13:07:48 -0400

    Adding and Correcting Vision of Abyssea Drops

---
+++ sql/zz_abyssea_mob_spawn_points.sql
@@ -0,0 +1,3 @@
+UPDATE mob_spawn_points SET pos_x = 253.389, pos_y = 39.500, pos_z = 346.867 WHERE mobid = 16961935;
+UPDATE mob_groups SET minLevel = 85, maxLevel = 85 WHERE poolid = 4054;
+UPDATE mob_groups SET minLevel = 85, maxLevel = 85 WHERE poolid = 3080;
