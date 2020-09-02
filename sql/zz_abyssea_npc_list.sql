c312ce07a0cab9b2c469dbd28d09dde21b74d1c5
Ian <ian.evans1188@gmail.com>
2020-09-02 11:00:00 -0400

    Correction to NPC look

--- sql/zz_abyssea_npc_list.sql
+++ sql/zz_abyssea_npc_list.sql
@@ -1,2 +1,2 @@
-UPDATE npc_list SET pos_rot = 192, pos_x = -486.1201, pos_y = 0.064, pos_z = 742.2906, status = 0 WHERE npcid = 17318618;
-UPDATE npc_list SET pos_rot = 58, pos_x = -32.9504, pos_y = 33.6785, pos_z = 182.0314, status = 0 WHERE npcid = 17318615;
+UPDATE npc_list SET pos_rot = 192, pos_x = -486.1201, pos_y = 0.064, pos_z = 742.2906, status = 0, look = 0x010003041C101C201C301C401C50006000700000 WHERE npcid = 17318618;
+UPDATE npc_list SET pos_rot = 58, pos_x = -32.9504, pos_y = 33.6785, pos_z = 182.0314, status = 0, look = 0x01000A0714101520083008400650006000700000 WHERE npcid = 17318615;
