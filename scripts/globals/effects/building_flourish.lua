4fa33e47698f09b52d3b1ecb0531684e438ae47d
Ian <ian.evans1188@gmail.com>
2020-09-06 19:47:13 -0400

    Building and Climactic Flourish Effects

--- scripts/globals/effects/building_flourish.lua
+++ scripts/globals/effects/building_flourish.lua
@@ -3,12 +3,50 @@
 -- tpz.effect.BUILDING_FLOURISH
 --
 -----------------------------------
+require("scripts/globals/status")
+-----------------------------------
 
 function onEffectGain(target, effect)
+	local BF_EFFECT = effect:getPower()
+	
+	if BF_EFFECT == 1 then
+	    target:addMod(tpz.mod.ACC, effect:getPower())
+	    target:addMod(tpz.mod.ACC, 25)
+	elseif BF_EFFECT == 2 then
+	    target:addMod(tpz.mod.ACC, effect:getPower())
+	    target:addMod(tpz.mod.ACC, 25)
+	    target:addMod(tpz.mod.ATTP, effect:getPower())
+	    target:addMod(tpz.mod.ATTP, 25)
+	elseif BF_EFFECT == 3 then
+	    target:addMod(tpz.mod.ACC, effect:getPower())
+	    target:addMod(tpz.mod.ACC, 25)
+	    target:addMod(tpz.mod.ATTP, effect:getPower())
+	    target:addMod(tpz.mod.ATTP, 25)
+		target:addMod(tpz.mod.CRITHITRATE, effect:getPower())
+	    target:addMod(tpz.mod.CRITHITRATE, 25)
+	end
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
+	local BF_EFFECT = effect:getPower()
+	
+	if BF_EFFECT == 1 then
+	    target:delMod(tpz.mod.ACC, effect:getPower())
+	    target:delMod(tpz.mod.ACC, 25)
+	elseif BF_EFFECT == 2 then
+	    target:delMod(tpz.mod.ACC, effect:getPower())
+	    target:delMod(tpz.mod.ACC, 25)
+	    target:delMod(tpz.mod.ATTP, effect:getPower())
+	    target:delMod(tpz.mod.ATTP, 25)
+	elseif BF_EFFECT == 3 then
+	    target:delMod(tpz.mod.ACC, effect:getPower())
+	    target:delMod(tpz.mod.ACC, 25)
+	    target:delMod(tpz.mod.ATTP, effect:getPower())
+	    target:delMod(tpz.mod.ATTP, 25)
+		target:delMod(tpz.mod.CRITHITRATE, effect:getPower())
+	    target:delMod(tpz.mod.CRITHITRATE, 25)
+	end
 end
--- scripts/globals/effects/climactic_flourish.lua
+++ scripts/globals/effects/climactic_flourish.lua
@@ -3,8 +3,28 @@
 -- tpz.effect.CLIMACTIC_FLOURISH
 --
 -----------------------------------
+require("scripts/globals/status")
+-----------------------------------
 
 function onEffectGain(target, effect)
+	local CF_EFFECT = effect:getPower()
+	
+	if CF_EFFECT == 1 then
+		target:addMod(tpz.mod.CRITHITRATE, effect:getPower())
+	    target:addMod(tpz.mod.CRITHITRATE, 100)
+	elseif CF_EFFECT == 2 then
+		target:addMod(tpz.mod.CRITHITRATE, effect:getPower())
+	    target:addMod(tpz.mod.CRITHITRATE, 100)
+	elseif CF_EFFECT == 3 then
+		target:addMod(tpz.mod.CRITHITRATE, effect:getPower())
+	    target:addMod(tpz.mod.CRITHITRATE, 100)
+	elseif CF_EFFECT == 4 then
+		target:addMod(tpz.mod.CRITHITRATE, effect:getPower())
+	    target:addMod(tpz.mod.CRITHITRATE, 100)
+	elseif CF_EFFECT == 5 then
+		target:addMod(tpz.mod.CRITHITRATE, effect:getPower())
+	    target:addMod(tpz.mod.CRITHITRATE, 100)
+	end
 end
 
 function onEffectTick(target, effect)
